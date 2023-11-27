97909b0636d3175f113653353ced22eebe8c607d
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-11-27 18:30:13 +0000

    used ref to check rating decision

--- usepopcorn2/src/App.js
+++ usepopcorn2/src/App.js
@@ -200,6 +200,12 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
 
+  const countRef = useRef(0);
+
+  useEffect(() => {
+    if (userRating) countRef.current = countRef.current + 1;
+  }, [userRating]);
+
   const isWatched = watched.map((movie) => movie.imdbId).includes(selectedId);
 
   const watchedUserRating = watched.find(
@@ -260,6 +266,7 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
       imdbRating: Number(imdbRating),
       userRating,
       runtime: Number(runtime.split(" ").at(0)),
+      countRatingDecisions: countRef.current,
     };
 
     onAddWatched(newMovieWatched);
