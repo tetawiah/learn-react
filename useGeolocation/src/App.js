9a9d1e450cd479f41f691d58cde075f8cedf0139
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-12-01 16:02:39 +0000

    code fix

--- useGeolocation/src/App.js
+++ useGeolocation/src/App.js
@@ -4,17 +4,18 @@ import {useGeolocation} from "./useGeolocation";
 
 export default function App() {
     const [count,setCount] = useState(0);
-    const[position,setPosition,isLoading,error] = useGeolocation();
+    const[position,isLoading,error,getPosition] = useGeolocation();
     const {lat,lng} = position;
     const handlePositionChange = () => {
         setCount(count=> count+1);
+       getPosition();
     }
   return (
       <div>
-        <button onClick={handlePositionChange}>Get my position</button>
+        <button onClick={handlePositionChange} disabled={isLoading}>Get my position</button>
         <DisplayText> {error ? <p>error</p> :
             isLoading ? 'Loading position...' : Object.keys(position).length > 0 && `Your GPS position ${lat} ${lng}`}</DisplayText>
-        <DisplayText>You requested position {count || false} times</DisplayText>
+        <DisplayText>You requested position {count} times</DisplayText>
       </div>
 
 )
--- useGeolocation/src/useGeolocation.js
+++ useGeolocation/src/useGeolocation.js
@@ -1,28 +1,29 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 export function useGeolocation() {
     const [position,setPosition] = useState({});
     const [error,setError] = useState("");
     const[isLoading,setIsLoading] = useState(false);
 
-    useEffect(() => {
-        if (!navigator.geolocation)
-            return setError("Your browser does not support geolocation");
+        function getPosition() {
+            if (!navigator.geolocation)
+                return setError("Your browser does not support geolocation");
 
-        setError("");
-        setIsLoading(true);
-        navigator.geolocation.getCurrentPosition(
-            (pos) => {
-                setPosition({
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                });
-    },
-            (e) => {console.log(error)
-            setError(e.message)});
-        setIsLoading(false);
-},[])
+            setError("");
+            setIsLoading(true);
+            navigator.geolocation.getCurrentPosition(
+                (pos) => {
+                    setPosition({
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude
+                    });
+                },
+                (e) => {console.log(error)
+                    setError(e.message)});
+            setIsLoading(false);
+        }
 
-return [position,setPosition,isLoading,error];
+
+return [position,isLoading,error,getPosition];
 
 }
