73220333894f71f1397bc21d99bd585968b9e6d7
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-12-02 07:52:27 +0000

    -added quiz status -added start screen -added loader -added error

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,14 +1,56 @@
 import Header from "./Header";
 import Main from "./Main";
-import {useEffect} from "react";
+import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = { questions: [], status: "loading" };
+//'loading','error','ready','active','finished'
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
-    useEffect(() => {
-        fetch('http://localhost:8001/questions').then(res=>res.json())
-            .then(data=>console.log(data));
-    }, []);
-    return <div>
-        <Header/>
-        <Main></Main>
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numOfQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8001/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numOfQuestions={numOfQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
+      </Main>
     </div>
+  );
 }
