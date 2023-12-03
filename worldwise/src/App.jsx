7d28ab199b19d2fa65c734b0b3706a5d1996a782
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-12-03 23:02:42 +0000

    starter

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,16 +2,18 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import Product from "./pages/Product.jsx";
+import "./index.css";
+import Login from "./pages/Login.jsx";
 export default function App(){
-    return <div>
+    return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Homepage/>}> </Route>
                 <Route path="/product" element={<Product/>}> </Route>
                 <Route path="/pricing" element={<Pricing/>}> </Route>
+                <Route path="/login" element={<Login/>}> </Route>
             </Routes>
         </BrowserRouter>
-
-    </div>
+    )
 
 }
--- worldwise/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import {NavLink} from "react-router-dom";
-
-export default function Navbar () {
-    return <nav>
-        <NavLink to="/">Home </NavLink>
-        <NavLink to="/product" >Product </NavLink>
-        <NavLink to="/pricing" > Pricing </NavLink>
-    </nav>
-}
