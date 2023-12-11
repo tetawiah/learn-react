ed3e312c9924bf5444278bab8c3bb8ac8d77f289
tetawiah <tawiah.tetteh10@gmail.com>
2023-12-11 13:53:22 +0000

    starter

---
+++ redux-intro/src/App.js
@@ -0,0 +1,19 @@
+import CreateCustomer from "./CreateCustomer";
+import Customer from "./Customer";
+import AccountOperations from "./AccountOperations";
+import BalanceDisplay from "./BalanceDisplay";
+import "./store";
+
+function App() {
+  return (
+    <div>
+      <h1>🏦 The React-Redux Bank ⚛️</h1>
+      <CreateCustomer />
+      <Customer />
+      <AccountOperations />
+      <BalanceDisplay />
+    </div>
+  );
+}
+
+export default App;
