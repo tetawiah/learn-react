82e403df6b3d9e72f83c4b5f50561dd0cc8db394
tetawiah <tawiah.tetteh10@gmail.com>
2023-12-12 10:30:07 +0000

    -refactored to react toolkit -used custom action creator"

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -1,41 +1,47 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        createdAt: action.payload.createdAt,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return;
-      return {
-        ...state,
-        loan: action.payload.amt,
-        loanPurpose: action.payload.pur,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        balance: state.balance - state.loan,
-        loanPurpose: "",
-      };
-    case "account/convertingCurrency":
-      return { ...state, isLoading: true };
-    default:
-      return state;
-  }
-}
-
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amt, pur) {
+        return {
+          payload: {
+            amt,
+            pur,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amt;
+        state.loanPurpose = action.payload.pur;
+        state.balance += state.loan;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 export function deposit(amount, currency) {
   if (currency === "USD")
     return {
@@ -58,26 +64,62 @@ export function deposit(amount, currency) {
     });
   };
 }
+export default accountSlice.reducer;
 
-export function withdraw(amount) {
-  return {
-    type: "account/withdraw",
-    payload: amount,
-  };
-}
+//
+// export default function accountReducer(state = initialStateAccount, action) {
+//   switch (action.type) {
+//     case "account/deposit":
+//       return {
+//         ...state,
+//         balance: state.balance + action.payload,
+//         createdAt: action.payload.createdAt,
+//       };
+//     case "account/withdraw":
+//       return { ...state, balance: state.balance - action.payload };
+//     case "account/requestLoan":
+//       if (state.loan > 0) return;
+//       return {
+//         ...state,
+//         loan: action.payload.amt,
+//         loanPurpose: action.payload.pur,
+//       };
+//     case "account/payLoan":
+//       return {
+//         ...state,
+//         loan: 0,
+//         balance: state.balance - state.loan,
+//         loanPurpose: "",
+//       };
+//     case "account/convertingCurrency":
+//       return { ...state, isLoading: true };
+//     default:
+//       return state;
+//   }
+// }
+// //
 
-export function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: {
-      amt: amount,
-      pur: purpose,
-    },
-  };
-}
-
-export function payLoan() {
-  return {
-    type: "account/payLoan",
-  };
-}
+//
+// export function withdraw(amount) {
+//   return {
+//     type: "account/withdraw",
+//     payload: amount,
+//   };
+// }
+//
+// export function requestLoan(amount, purpose) {
+//   return {
+//     type: "account/requestLoan",
+//     payload: {
+//       amt: amount,
+//       pur: purpose,
+//     },
+//   };
+// }
+//
+// export function payLoan() {
+//   return {
+//     type: "account/payLoan",
+//   };
+// }
+//
